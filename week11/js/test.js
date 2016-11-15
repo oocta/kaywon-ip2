127ca608199e50b3f3aeb4a2337009602e69e130
Kwangsoo Shin <dalgos@wemakeprice.com>
2016-11-15 21:31:50 +0900

    step 1 기본 함수 생성

--- week11/js/test.js
+++ week11/js/test.js
@@ -1,1 +1,30 @@
-$('#container').html('<h1>나 로드 됐지롱!</h1>');
+//Ajax test code.
+function getAjaxData(url) {
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    async: true,
+    url: url,
+    success: successHandler,
+    error: errorHandler
+  });
+}
+
+function successHandler(res) {
+  console.log(res);
+}
+
+function errorHandler(err) {
+  console.log(err);
+  throw new Error(err.responseText);
+}
+
+$(function () {
+  
+  var $gnb = $('#gnb');
+  var $container = $('#container');
+  
+  //Ajax load Test
+  getAjaxData('json/appData.jsons');
+  
+});
