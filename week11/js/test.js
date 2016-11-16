223fb00e197f0c4820c7d5d5ca1416f2b457bbe0
Kwangsoo Shin <dalgos@wemakeprice.com>
2016-11-16 14:36:16 +0900

    week11 paging with scrollbar

--- week11/js/test.js
+++ week11/js/test.js
@@ -1,7 +1,85 @@
+//css version
+// $(function () {
+//   
+//   var $gnb = $('#gnb');
+//   var $container = $('#container');
+//   
+//   //Ajax load Test
+//   getAjaxData('json/appData.json');
+//   
+//   //Ajax test code.
+// 
+//   function getAjaxData(url) {
+//     $.ajax({
+//       type: 'GET',
+//       dataType: 'json',
+//       async: true,
+//       url: url,
+//       success: successHandler,
+//       error: errorHandler
+//     });
+//   }
+// 
+//   function successHandler(res) {
+//     
+//     var menuArray = res.menus;
+//     var sectionArray = [];
+//     
+//     $.map(menuArray, function (val, idx) {
+//       console.log(arguments)
+//       
+//       //GNB 버튼 추가
+//       var $button = $('<button/>', {text: val.name});
+//       $button.on('click', function () {
+//         window.scrollTo(0, window.inner);
+//       });
+//       $gnb.append($button);
+//       
+//       //Section 추가
+//       var zIndex = parseInt((menuArray.length - idx) * 1000);
+//       var $section = $('<section/>', {'class': val.class, style: 'z-index: ' + zIndex});
+//       var $h1 = $('<h1/>', {text: 'Section ' + (idx + 1)});
+//       $section.append($h1);
+//       
+//       $container.append($section);
+//       sectionArray.push($section);
+//     });
+//     
+//     //body height 확장
+//     var $body = $('body');
+//     $body.height(window.innerHeight * menuArray.length);
+//     
+//     $(window).on('scroll', function () {
+//       
+//       var percent = parseInt($(window).scrollTop() / ($body.height() - window.innerHeight) * 100, 10);
+//       var sectionIndex = Math.min(parseInt(percent / 25), 3);
+//       
+//       $.map(sectionArray, function ($val, idx) {
+//         if (idx + 1 > sectionIndex) {
+//           $val.removeClass('upper');
+//         } else {
+//           if (!$val.hasClass('upper')) {
+//             $val.addClass('upper');
+//           };
+//         }
+//       });
+//       
+//     }).trigger('scroll');
+//   }
+// 
+//   function errorHandler(err) {
+//     console.log(err);
+//   }
+//   
+// });
+
+// animate version
 $(function () {
   
   var $gnb = $('#gnb');
   var $container = $('#container');
+  var $body = $('body');
+  var sectionArray = [];
   
   //Ajax load Test
   getAjaxData('json/appData.json');
@@ -22,10 +100,8 @@ $(function () {
   function successHandler(res) {
     
     var menuArray = res.menus;
-    var sectionArray = [];
     
     $.map(menuArray, function (val, idx) {
-      console.log(arguments)
       
       //GNB 버튼 추가
       var $button = $('<button/>', {text: val.name});
@@ -45,42 +121,61 @@ $(function () {
     });
     
     //body height 확장
-    var $body = $('body');
+    
     $body.height(window.innerHeight * menuArray.length);
     
-    var isScroll = false;
-    var beforeScrollTop = 0;
+    var maxDelta = 0;
+    var isMax = true;
     
-    $(window).on('scroll', function () {
-      
+    $(window).on('wheel', function wheelHandler(evt) {
       isScroll = true;
-      if (beforeScrollTop === $(window).scrollTop()) {
-        isSCroll = false;
-      } else {
-        beforeScrollTop = $(window).scrollTop();
-      }
-      console.log(beforeScrollTop, $(window).scrollTop(), isScroll);
-      
+      var delta = evt.originalEvent.wheelDelta;
       var percent = parseInt($(window).scrollTop() / ($body.height() - window.innerHeight) * 100, 10);
       var sectionIndex = Math.min(parseInt(percent / 25), 3);
+      if (delta > 0) {
+        if (maxDelta > delta) {
+          isMax = true;
+        } else {
+          maxDelta = delta;
+          return false;
+        }
+      } else {
+        if (maxDelta < delta) {
+          isMax = true;
+        } else {
+          maxDelta = delta;
+          return false;
+        }
+      }
       
-      // $.map(sectionArray, function ($val, idx) {
-      //   var targetTop = 0;
-      //   if (idx + 1 > sectionIndex) {
-      //     targetTop = window.innerHeight * -1;
-      //   } else {
-      //     targetTop = 0;
-      //   }
-      //   $val.stop().animate({top: targetTop, complete: function () {
-      //     $val.prop('isAnimate', false);
-      //   }});
-      // });
+      $(window).off('wheel');
+      playSection(sectionIndex, function () {
+        $(window).on('wheel', wheelHandler);
+      });
       
+      console.log(maxDelta, delta, sectionIndex);
     });
   }
 
   function errorHandler(err) {
     console.log(err);
+  }
+  
+  function playSection(index, completeCallback) {
+    console.log('playSection');
+    $.map(sectionArray, function ($section, idx) {
+      var topVal = '0';
+      if (idx < index) {
+        topVal = '-100%';
+      } else {
+        topVal = '0';
+      }
+      $section.stop().animate({top: topVal}, {complete: function () {
+        if (idx === index) {
+          completeCallback();
+        }
+      }});
+    });
   }
   
 });
